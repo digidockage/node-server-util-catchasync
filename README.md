8dfbe743dbe0f67bf924baf63e0c29a878569a42
Sgobbi Federico <federico@sgobbi.it>
2021-09-27 22:42:21 +0200

    First release

---
+++ README.md
@@ -0,0 +1,7 @@
+# Nodejs server util catchAsync
+Nodejs common server util catchAsync.
+
+### Install
+```
+npm install @esultocom/node-server-util-catchasync
+```
---
+++ src/index.js
@@ -0,0 +1,17 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                                 *
+ *    Copyright (c) 2021 Sgobbi Federico                                                           *
+ *    All rights reserved.                                                                         *
+ *                                                                                                 *
+ *    This file is licensed under the MIT License.                                                 *
+ *    License text available at https://opensource.org/licenses/MIT                                *
+ *                                                                                                 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+// * * * * * * * * The code
+const catchAsync = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+};
+
+// * * * * * * * * Module export
+module.exports = catchAsync;
